9ad3f774ee21c60a63d692aa94bd7c0cdef9dc16
HAYASHI Kentaro <kenhys@gmail.com>
2010-05-18 21:32:15 +0900

    add use.

--- lib/messagepack.scm
+++ lib/messagepack.scm
@@ -3,6 +3,12 @@
 ;;;
 
 (define-module messagepack
+  (use messagepack.object)
+  (use messagepack.pack)
+  (use messagepack.sbuffer)
+  (use messagepack.unpack)
+  (use messagepack.vrebuffer)
+  (use messagepack.zone)
   (export test-messagepack ;; dummy
           )
   )
