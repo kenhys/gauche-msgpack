1588453c401284ff2cf4792cc274c3baf48e45b3
HAYASHI Kentaro <kenhys@gmail.com>
2012-02-24 23:03:52 +0900

    add hello-messagepack.scm

---
+++ examples/hello-messagepack.scm
@@ -0,0 +1,31 @@
+;; -*- mode:scheme; coding: utf-8 -*-
+
+(use gauche.interactive)
+(use msgpack)
+
+
+(define (main args)
+  ([buffer (sbuffer-new)]
+   [pk (packer-new buffer msgpack-sbuffer-write)])
+  
+  ;; serialized ["Hello", "MessagePack"]
+  (pack-array pk 2)
+  (pack-raw pk 5)
+  (pack-raw-body pk "Hello" 5)
+  (pack-raw pk 11)
+  (pack-raw-body pk "MessagePack" 11)
+
+  ;; deserialize it.
+  (let* ([msg (make <msgpack-unpacked>)]
+	 [obj (make <msgpack-object>)])
+    (unpacked-init msg)
+    
+    (unpack-next msg buffer)
+    
+    ;; prints
+    (set! obj (slot-ref! msg 'data))
+    (object-print stdout obj))
+
+  (free buffer)
+  (free pk))
+	 
