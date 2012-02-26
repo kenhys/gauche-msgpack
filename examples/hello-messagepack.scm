c853918e962cdd989e8fc494407e3cb5024c8556
HAYASHI Kentaro <kenhys@gmail.com>
2012-02-26 20:09:48 +0900

    use (cast

--- examples/hello-messagepack.scm
+++ examples/hello-messagepack.scm
@@ -12,21 +12,30 @@
     
     (d buffer)
     (d pk)
+    
     ;; serialized ["Hello", "MessagePack"]
-    (pack-array pk 2)
+    (d (pack-array pk 2))
+
+    (d buffer)
+    
     (pack-raw pk 5)
+
+    (d buffer)
+
     (pack-raw-body pk "Hello" 5)
     (pack-raw pk 11)
     (pack-raw-body pk "MessagePack" 11)
-
+    (exit)
+    (d pk)
     ;; deserialize it.
     (let* ([msg (make <msgpack-unpacked>)]
 	   [obj (make <msgpack-object>)])
       (unpacked-init msg)
       
       (unpack-next msg buffer)
-      (exit)
+
       
+
       ;; prints
       (set! obj (slot-ref! msg 'data))
       (object-print stdout obj))
